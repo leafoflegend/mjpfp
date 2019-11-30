e4d7670601d5795d19fd4236172e175ad3cb181d
Mark <m.bae@live.com>
2019-11-30 17:18:37 -0500

    fixed webpack issues

--- client/app.js
+++ client/app.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 
 const root = document.querySelector('#root');
 
-class App extends React.component{
-    render(){
-        console.log('testing')
-        return (
-            <h1>TESTING WEBPACK</h1>
-        )
-    }
+class App extends React.Component {
+  render() {
+    console.log('testing');
+    return <h1>TESTING WEBPACK</h1>;
+  }
 }
 ReactDOM.render(<App />, root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    entry: './client/app.js',
-    module: {
-        rules: [
-            {
-                test: /\.js/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    }
+  entry: './client/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
